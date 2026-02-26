b4d68a315b5536a959fe7bc7f3f6fe56453cc8bd
225662 <cleopatra.mwajombe@strathmore.edu>
2026-02-26 10:45:13 +0300

    ABSTRACT CLASS

---
+++ src/interfaceClass.java
@@ -0,0 +1,13 @@
+public class interfaceClass {
+    //interface 1
+interface Borrowable{
+    void borrowableItem();
+    void returnItem();
+
+}
+
+//interface 2
+interface Payable{
+    double calculateFee();
+
+}
